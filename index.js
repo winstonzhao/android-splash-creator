1ce8e540c297beab6911b14cbc2951c843f43cd4
Winston Zhao <winstonzhao@outlook.co.nz>
2019-11-20 21:38:23 +1300

    refactor

--- index.js
+++ index.js
@@ -5,47 +5,49 @@ const Listr = require("listr");
 const program = require("commander");
 const takeScreenshot = require("./src/main");
 
-program
-  .option("-p, --port <number>", "localhost port", parseInt)
-  .option(
-    "-u, --url <string>",
-    "url of webpage, if url is passed, port is ignored"
-  );
+(async () => {
+  program
+    .option("-p, --port <number>", "localhost port", parseInt)
+    .option(
+      "-u, --url <string>",
+      "url of webpage, if url is passed, port is ignored"
+    );
 
-program.parse(process.argv);
+  program.parse(process.argv);
 
-let url;
+  let url;
 
-if (program.port) {
-  url = `http://localhost:${program.port}/`;
-}
-
-if (program.url) {
-  url = program.url;
-}
-
-if (!url) {
-  url = `http://localhost:3333/`;
-}
-
-console.log(chalk.green(boxen("starting", { padding: 1, margin: 2 })));
-
-const tasks = new Listr([
-  {
-    title: chalk.white.italic("working..."),
-    task: () => {
-      return new Observable(observer => {
-        takeScreenshot(url, observer).catch(err => {
-          observer.error(err);
-        });
-      });
-    }
+  if (program.port) {
+    url = `http://localhost:${program.port}/`;
   }
-]);
 
-tasks
-  .run()
-  .catch(process.exit)
-  .then(() =>
-    console.log(chalk.green(boxen("  done  ", { padding: 1, margin: 2 })))
-  );
+  if (program.url) {
+    url = program.url;
+  }
+
+  if (!url) {
+    url = `http://localhost:3333/`;
+  }
+
+  console.log(chalk.green(boxen("starting", { padding: 1, margin: 2 })));
+
+  const tasks = new Listr([
+    {
+      title: chalk.white.italic("working..."),
+      task: () => {
+        return new Observable(observer => {
+          takeScreenshot(url, observer).catch(err => {
+            observer.error(err);
+          });
+        });
+      }
+    }
+  ]);
+
+  try {
+    await tasks.run();
+    console.log(chalk.green(boxen("  done  ", { padding: 1, margin: 2 })));
+  } catch {
+    process.exit();
+  }
+})();
